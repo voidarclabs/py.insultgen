043fef1ffc1bedb7c4468a61c84074df155bbcb1
voidarc <dominicbourne77@gmail.com>
2023-04-18 16:12:12 +0000

    v1

---
+++ rude.py
@@ -0,0 +1,16 @@
+import time
+import os
+import random
+
+def insult(num):
+    adj = open("adj.txt", "r")
+    ver = open("ver.txt", "r")
+    nou = open("nou.txt", "r")
+    x = adj.read().splitlines()
+    y = ver.read().splitlines()
+    z = nou.read().splitlines()
+
+    for i in range(num):
+        print("you're a" + random.choice(x).lower() + " " + random.choice(y).lower() + " " + random.choice(z).lower() + ".")
+    
+
