61fa22979e80f2dc5ec9a6a3d71939544446b483
voidarc <dominicbourne77@gmail.com>
2023-05-04 20:33:39 +0000

    bracnh changes

--- rude.py
+++ rude.py
@@ -3,13 +3,12 @@ import os
 import random
 
 def insult(num):
-    for i in range(num):
-        adj = open("adj.txt", "r")
-        ver = open("ver.txt", "r")
-        nou = open("nou.txt", "r")
-        x = adj.read().splitlines()
-        y = ver.read().splitlines()
-        z = nou.read().splitlines()
-        print("you're a" + random.choice(x).lower() + " " + random.choice(y).lower() + " " + random.choice(z).lower() + ".")
-    
+    adj = open("adj.txt", "r")
+    ver = open("ver.txt", "r")
+    nou = open("nou.txt", "r")
+    x = adj.read().splitlines()
+    y = ver.read().splitlines()
+    z = nou.read().splitlines()
+        
+return ("you're a" + random.choice(x).lower() + " " + random.choice(y).lower() + " " + random.choice(z).lower() + ".")
 
